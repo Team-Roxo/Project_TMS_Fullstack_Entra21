62b5483ed2aab510db6445a9b98f6d21fb0cffe6
kaka-jaques <kjfakhouri@gmail.com>
2022-08-29 22:46:03 -0300

    UPDATE
    Administrator Menu created!

---
+++ backend/TMSFullstackProject/src/main/java/br/com/teamroxo/entra21/TMSFullstackProject/TmsFullstackProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.teamroxo.entra21.TMSFullstackProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsFullstackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsFullstackProjectApplication.class, args);
+	}
+
+}
---
+++ backend/TMSFullstackProject/src/test/java/br/com/teamroxo/entra21/TMSFullstackProject/TmsFullstackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teamroxo.entra21.TMSFullstackProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsFullstackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
