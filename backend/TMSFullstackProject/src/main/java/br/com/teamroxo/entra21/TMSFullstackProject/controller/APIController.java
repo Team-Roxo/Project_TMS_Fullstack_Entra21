1352990df53a6b726f1e24def1d39e5f2ed81d33
Kalil J. Fakhouri <kalil@showtec.local>
2022-08-31 17:01:59 -0300

    UPDATE
    APIController Created!

---
+++ backend/TMSFullstackProject/src/main/java/br/com/teamroxo/entra21/TMSFullstackProject/controller/APIController.java
@@ -0,0 +1,12 @@
+package br.com.teamroxo.entra21.TMSFullstackProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tms")
+public class APIController {
+
+	
+	
+}
