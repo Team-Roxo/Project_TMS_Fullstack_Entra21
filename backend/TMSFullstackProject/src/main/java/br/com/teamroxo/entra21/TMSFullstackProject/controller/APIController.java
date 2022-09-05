030cc778c4622b1c63186241c6625e5c1b4c12b8
kaka-jaques <kjfakhouri@gmail.com>
2022-09-04 23:06:18 -0300

    UPDATE!
    API INITIALIZE
    SIDEBAR ANIMATION FIXED

--- backend/TMSFullstackProject/src/main/java/br/com/teamroxo/entra21/TMSFullstackProject/controller/APIController.java
+++ backend/TMSFullstackProject/src/main/java/br/com/teamroxo/entra21/TMSFullstackProject/controller/APIController.java
@@ -1,12 +1,35 @@
 package br.com.teamroxo.entra21.TMSFullstackProject.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.teamroxo.entra21.TMSFullstackProject.database.PacoteDados;
+import br.com.teamroxo.entra21.TMSFullstackProject.template.Pacotes;
 
 @RestController
 @RequestMapping("/tms")
 public class APIController {
 
+	@Autowired
+	private PacoteDados pacoteDados;
 	
+	@GetMapping(value = "/all")
+	public List<Pacotes> getAll(){
+		return pacoteDados.findAll();
+	}
+	
+	@PostMapping(value = "/add")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Pacotes post(@RequestBody Pacotes pacotes) {
+		return pacoteDados.save(pacotes);
+	}
 	
 }
