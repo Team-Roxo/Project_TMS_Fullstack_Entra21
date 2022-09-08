cf542a2e600d2736cf7e6331716b11c1e7b8f023
kaka-jaques <kjfakhouri@gmail.com>
2022-09-08 20:58:32 -0300

    UPDATE!
    ROUTERLINKS OF HOME WORKS!

---
+++ backend/TMSProject/src/main/java/br/com/entra21/teamroxo/TMSProject/TmsProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.entra21.teamroxo.TMSProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsProjectApplication.class, args);
+	}
+
+}
