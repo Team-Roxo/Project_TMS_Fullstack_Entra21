91fb4e6974f4d429d6e5c2133a9b854656dfa2e2
kaka-jaques <kjfakhouri@gmail.com>
2022-09-10 20:06:40 -0300

    BIG UPDATE!

--- frontend/main/src/app/loginservice.service.ts
+++ frontend/main/src/app/loginservice.service.ts
@@ -13,10 +13,12 @@ export class LoginserviceService implements CanActivate {
   user!:string
   TMSLoginAPI:string = "https://randomuser.me/api/?results=1"
   succeed!:boolean
+  progress:boolean = false
 
   constructor(private router:Router, private http:HttpClient) {}
 
   logging(user:string, password:string){
+    this.progress = true
     fetch(this.TMSLoginAPI)
     .then((resp) => resp.json())
     .then((data) =>{
@@ -27,15 +29,33 @@ export class LoginserviceService implements CanActivate {
         new LoginComponent(this.router, this).gotoHome()
       })
     })
-    .catch(function(error){
+    .catch((error) =>{
       console.log(error);
+      this.progress = false
     })
 
     return this.http.get<any>(this.TMSLoginAPI+'/'+user+'/'+password)
   }
 
   registering(user:string, password:string, email:string){
-    this.user = user
+    this.progress = true
+    fetch(this.TMSLoginAPI)
+    .then((resp) => resp.json())
+    .then((data) =>{
+      let ranUsers = data.results;
+      return ranUsers.map((ranUser:any) =>{
+        this.user = `${ranUser.name.first}`
+        this.succeed = true
+        new LoginComponent(this.router, this).gotoHome()
+      })
+    })
+    .catch((error) =>{
+      console.log(error);
+      this.progress = false
+    })
+
+    return this.http.get<any>(this.TMSLoginAPI+'/'+user+'/'+password)
+    
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
