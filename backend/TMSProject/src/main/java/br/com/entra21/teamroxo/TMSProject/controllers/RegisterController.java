5c84ccb317e2f96e8c8e8403978ec9e16f56a53c
kaka-jaques <kjfakhouri@gmail.com>
2022-09-23 07:15:54 -0300

    UPDATE!
    API CONNECT ON ONLINE DB!
    LOGIN API - WORKING!
    REGISTERING API - LOADING...

--- backend/TMSProject/src/main/java/br/com/entra21/teamroxo/TMSProject/controllers/RegisterController.java
+++ backend/TMSProject/src/main/java/br/com/entra21/teamroxo/TMSProject/controllers/RegisterController.java
@@ -1,5 +1,7 @@
 package br.com.entra21.teamroxo.TMSProject.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.entra21.teamroxo.TMSProject.interfaces.LoginRepository;
 import br.com.entra21.teamroxo.TMSProject.interfaces.PessoaRepository;
+import br.com.entra21.teamroxo.TMSProject.template.Login;
 import br.com.entra21.teamroxo.TMSProject.template.Pessoa;
 
 @RestController
@@ -23,10 +27,23 @@ public class RegisterController {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	
+	@Autowired
+	private LoginRepository loginRepository;
+	
 	@PostMapping()
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public @ResponseBody Pessoa register(@RequestBody Pessoa credentials){
+		
 		return pessoaRepository.save(credentials);
+		
+	}
+	
+	@PostMapping("/login")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Login registerLogin(@RequestBody Login credentials) {
+		
+		return loginRepository.save(credentials);
+		
 	}
 	
 }
