d2a21f4e2ab5285675dcfb1215e48c4d0b65a4da
bruno-roberto <brunorobertopds@gmail.com>
2022-10-05 00:58:04 -0300

    Component - Visit Relatório Aniversariantes Rotas Dashboard

--- frontend/main/src/app/body/body.component.ts
+++ frontend/main/src/app/body/body.component.ts
@@ -12,42 +12,55 @@ import { Observable } from 'rxjs';
 })
 export class BodyComponent implements OnInit {
 
-  readonly APIBirthNow: string = "http://localhost:8080/user/birthnow" 
- 
-  readonly APIBirthMonth: string = "http://localhost:8080/user/birthmonth" 
+  readonly APIBirthNow: string = "http://localhost:8080/user/birthnow"
+
+  readonly APIBirthMonth: string = "http://localhost:8080/user/birthmonth"
 
   birthnow! : string
   birthmonth! : string
   aniversariantes!: Array<any>
+  aniversariantesMes!: Array<any>
   id!: number
   nome!: string
-  email!: string 
+  email!: string
   birth!: Date
   document!: string
 
   constructor(public loginService:LoginserviceService, private router:Router, private http:HttpClient) { }
 
   ngOnInit(): void {
+
+    this.aniversariantes = new Array()
+    this.aniversariantesMes = new Array()
+
     this.http.get(this.APIBirthNow)
     .subscribe((resultado:any) => {
-     this.birthnow = resultado
+
+      var count = Object.keys(resultado).length
+
+      for(let i=0;i<count;i++){
+
+        this.aniversariantes.push({id: resultado[i].id ,nome: resultado[i].nome, email: resultado[i].email, birth: resultado[i].birth, document: resultado[i].document})
+
+      }
+
     });
 
     this.http.get(this.APIBirthMonth)
     .subscribe((resultado:any) => {
-     this.birthmonth=resultado
+
+      var count = Object.keys(resultado).length
+
+      for(let i=0;i<count;i++){
+
+        this.aniversariantesMes.push({id: resultado[i].id ,nome: resultado[i].nome, email: resultado[i].email, birth: resultado[i].birth, document: resultado[i].document})
+
+      }
+
     });
 
   }
 
   sair(){
 
-    this.loginService.succeed = false;
-
-  }
-
-  aniversariante(){
-    this.aniversariantes.push ({id: this.id ,nome: this.nome, email: this.email, birth: this.birth, document: this.document})
-  }
-
-}
+    this.loginService.succeed = false;}}
