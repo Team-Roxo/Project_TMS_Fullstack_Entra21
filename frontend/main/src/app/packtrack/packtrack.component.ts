f7a98ae5dea0c868f528ee564c2d54e3d23e7aec
kaka-jaques <kjfakhouri@gmail.com>
2022-10-07 23:10:34 -0300

    UPDATE MODAL

--- frontend/main/src/app/packtrack/packtrack.component.ts
+++ frontend/main/src/app/packtrack/packtrack.component.ts
@@ -12,4 +12,27 @@ export class PacktrackComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openModal(){
+
+    var modal = document.getElementById('modal2')
+
+    modal?.setAttribute('style', 'display:block;')
+
+    modal?.setAttribute('class', 'portfolio-modal modal fade show')
+    modal?.removeAttribute('aria-hidden')
+
+    modal?.setAttribute('arial-modal', 'true')
+
+  }
+
+  closeModal(){
+    var modal = document.getElementById('modal2')
+
+    modal?.setAttribute('class', 'portfolio-modal modal fade')
+    setTimeout(()=>{
+      modal?.setAttribute('style', 'display:none;')
+    },500)
+
+  }
+
 }
