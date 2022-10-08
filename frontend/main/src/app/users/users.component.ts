8d42841125a0e4d554c095284b5ce94816ee034c
Mateus Felipe <mateusgoettems@gmail.com>
2022-10-08 10:20:03 -0300

    Updated Cadastro por pop up, (user, carrier e ship quote)

--- frontend/main/src/app/users/users.component.ts
+++ frontend/main/src/app/users/users.component.ts
@@ -69,6 +69,29 @@ birth!:string
   alterar(){
     
   }
+
+  openModal(){
+
+    var modal = document.getElementById('modal2')
+
+    modal?.setAttribute('style', 'display:block;')
+
+    modal?.setAttribute('class', 'portfolio-modal modal fade show')
+    modal?.removeAttribute('aria-hidden')
+
+    modal?.setAttribute('arial-modal', 'true')
+
+  }
+
+  closeModal(){
+    var modal = document.getElementById('modal2')
+
+    modal?.setAttribute('class', 'portfolio-modal modal fade')
+    setTimeout(()=>{
+      modal?.setAttribute('style', 'display:none;')
+    },500)
+
+  }
   
 
 }
