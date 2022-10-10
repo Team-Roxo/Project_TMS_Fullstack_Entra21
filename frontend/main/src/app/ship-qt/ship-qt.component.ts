0fd56b63d6d2863df17154fa6e6167b9b2d61857
kaka-jaques <kjfakhouri@gmail.com>
2022-10-09 23:11:21 -0300

    UPDATE I THINK

--- frontend/main/src/app/ship-qt/ship-qt.component.ts
+++ frontend/main/src/app/ship-qt/ship-qt.component.ts
@@ -47,7 +47,7 @@ export class ShipQtComponent implements OnInit {
 
   ngOnInit(): void {
 
-    
+
 
     //this.quotes.push({ precoFrete: 45.50, tempo: 3, trackid: "BR23154546TR", cepOrigem: 88058086, cepDestino: 88058086, comprimento: 50, largura: 50, altura: 50, peso: 10 })
     //this.quotes.push({ precoFrete: 94.50, tempo: 6, trackid: "BR22315445TR", cepOrigem: 46513265, cepDestino: 65898454, comprimento: 100, largura: 200, altura: 10, peso: 25 })
@@ -75,7 +75,12 @@ export class ShipQtComponent implements OnInit {
 
     if (this.cepOrigem != null && this.cepDestino != null && this.comprimento != null, this.altura != null, this.largura != null, this.peso != null) {
       this.quoteService.quote(this.cepOrigem, this.cepDestino)
-        .pipe()
+        .pipe(
+          catchError((error)=>{
+            // adicionar função caso dê erro
+            return error
+          }
+          ))
         .subscribe((response: any) => {
 
           console.log(response);
